e9cbbf0195ea6e29c0289cebcb3c58dc81af62ea
Alan Santos <alansantosmg@gmail.com>
2020-03-15 15:07:19 -0300

    Nova postagem.

--- blog-update.sh
+++ blog-update.sh
@@ -2,11 +2,14 @@
 cd ~/blog/github.com/alansantosmg
 sleep 3
 echo
-mydate=`date "+%F"`
+mydate=`date "+%F-%H%M%S"`
 tar -czvf $mydate-hugoblog.tar.gz ./hugoblog
+echo
 sleep 3
-cd ~/blog/github.com/alansantosmg/hugoblog
+echo
+cd hugoblog
 git add .
 git commit -m "Nova postagem."
 git push origin master
+sleep 3
 echo "Postagem realizada!"
