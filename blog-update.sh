ab48d952f476eba1935ecfdf4259fa552af782b3
alansantosmg <alansantosmg@hotmail.com>
2021-01-07 19:36:44 -0300

    Nova atualização.

--- blog-update.sh
+++ blog-update.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 cd $HOME/src
 sleep 3
-echo "IF"
 
 if [ ! -d "hugoBlogBkp" ]; then 
-    echo "Criando diretório..."
+    echo "Criando diretório:  $HOME/src/hugoBlogBkp"
     sleep 3
     mkdir hugoBlogBkp
 fi 
 
-echo "fim IF"
-echo
+echo "Iniciando backup!"
+sleep 3
 mydate=`date "+%F-%H%M%S"`
 tar -czvf $HOME/src/hugoBlogBkp/$mydate-hugoblog.tar.gz $HOME/src/github.com/alansantosmg/hugoblog
 echo
+echo "Backup concluido. Iniciando atualização do blog."
 sleep 3
 echo
 cd $HOME/src/github.com/alansantosmg/hugoblog
 git add .
-git commit -m "Nova postagem."
+git commit -m "Nova atualização."
 git push origin master
 sleep 3
-echo "Postagem realizada!"
+echo "Atualização realizada!"
