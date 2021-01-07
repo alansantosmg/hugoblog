d5da2ffecacda2a8833bb54655aa0f2e2358abdd
alansantosmg <alansantosmg@hotmail.com>
2021-01-07 19:06:45 -0300

    Nova postagem.

--- blog-update.sh
+++ blog-update.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 cd $HOME/src
 sleep 3
-if [ -d "$HOME/src/hugoBlogBkp" ];then
+echo "IF"
+if [ -e "$HOME/src/hugoBlogBkp" ]; then
     echo "Criando diretório..."
     sleep 3
     mkdir "$HOME/src/hugoBlogBkp"
 fi 
-
+echo "fim IF"
 echo
 mydate=`date "+%F-%H%M%S"`
 tar -czvf $HOME/src/hugoBlogBkp/$mydate-hugoblog.tar.gz $HOME/src/github.com/alansantosmg/hugoblog
